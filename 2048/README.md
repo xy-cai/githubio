06892033e9b74b60307c276ae18ae2a034bb86c1
Xingyang Cai <tifosi.cai@gmail.com>
2014-03-19 22:36:41 +0800

    fix bugs

--- 2048/README.md
+++ 2048/README.md
@@ -1,8 +1,8 @@
-# 终
-
 又一个 [2048](https://github.com/gabrielecirulli/2048) 的分支版本。
 
-我大概就是汉化了一下……
+我大概就是汉化了一下…
+
+wahaha easier 版本
 
 ![截图](screenshot.jpg "截图")
 
--- 2048/js/game_manager.js
+++ 2048/js/game_manager.js
@@ -153,7 +153,7 @@ GameManager.prototype.move = function (direction) {
                     self.score += merged.value;
 
                     // The mighty 2048 tile
-                    if (merged.value === 1048600) self.won = true;
+                    if (merged.value === 1048544) self.won = true;
                 } else {
                     self.moveTile(tile, positions.farthest);
                 }
